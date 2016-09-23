6b8007d742e51a566801759352995dbc69d233eb
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-23 14:10:02 -0400

    Working on first line, need to apply to all other lines

--- Program.cs
+++ Program.cs
@@ -10,6 +10,31 @@ namespace RollerCoaster3
     {
         static void Main(string[] args)
         {
+            string shakespeareString = "To be, or not to be: that is the question.";
+
+            //Whether 'tis nobler in the mind to suffer.
+            //The slings and arrows of outrageous fortune.
+            //Or to take arms against a sea of troubles.
+            //And by opposing end them, to die: to sleep.
+
+            char[] array = shakespeareString.ToCharArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                char let = array[i];
+                if ( i % 2 == 0)
+                    array[i] = char.ToUpper(let);
+
+                if (i % 2 != 0)
+                    array[i] = char.ToLower(let);    
+                
+            }
+            string result = new string(array);
+            Console.WriteLine(result);
+        }
+
+
+
         }
     }
-}
+
